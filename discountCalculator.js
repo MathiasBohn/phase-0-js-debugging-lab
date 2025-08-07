1f795f6c7e71db1304266ff8fe6e2643c1d9f25b
Mathias <mathiasbohndk@gmail.com>
2025-08-06 22:43:35 -0400

    Tests passed

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,19 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+
+    for (let i = 1; i <= quantity; i++) { 
         totalPrice += pricePerItem;
     }
 
     if (quantity >= 10) {
-        totalPrice *= 0.9;
+        totalPrice *= 0.9; // Apply 10% discount
     }
 
     return totalPrice;
 }
 
+let Test = calculateDiscountedPrice(10,10)
 
+console.log(Test)
 
 module.exports = calculateDiscountedPrice;
